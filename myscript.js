06da86b034b77b45903da4b699234c334dcba255
Daniel Blair <CMDann.Development@gmail.com>
2013-08-02 15:59:46 -0500

    Updated code
    Added additional options and made it easier to add other services.

--- myscript.js
+++ myscript.js
@@ -1,35 +1,51 @@
-// The websites and the affiliate tags
+// This will check for Amazon.ca Links and adds the affiliate code. You can add any other affilaite program by adding more enteries.
 
-var sites = {
-        amazonca:     { url: "amazon.ca", tag:"tag=cmdca05-20"}; 
+var configurations = {
+      amazonca : { 
+        rx: /^http.*?\.amazon.ca.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
+        params: [
+          { param: "tag", paramValue: "cmdca05-20" }
+        ]
+      },
     };
     
-	// add your tag to the url
     function addTag(info) {
         var tUrl = info.url;
         var r = { cancel: false };
         
         console.log("Inside addTag() "); 
         
-        for (x in sites) { 
-          if (tUrl.indexOf(sites[x].url) >= 0) { 
-            if (tUrl.indexOf(sites[x].tag) == -1 ) {    
-              r = { redirectUrl: tUrl+(tUrl.indexOf("?") == -1 ? "?" : "&")+sites[x].tag };
-              chrome.windows.getCurrent(function (currentWindow) {
-				chrome.tabs.query({active: true, windowId: currentWindow.id}, function(tabs) {
-					chrome.pageAction.show(tabs[0].id);
-				 });
-			  });
-              break;
+        for ( var config in configurations) { 
+          if( configurations.hasOwnProperty(config) ) {
+            if (tUrl.match(configurations[config].rx) ) { 
+              if (tUrl.indexOf(configurations[config].params[0].param) == -1 ) {    
+                r = { redirectUrl: tUrl+(tUrl.indexOf("?") == -1 ? "?" : "&") + createTag(configurations[config].params) };
+                chrome.windows.getCurrent(function (currentWindow) {
+                  chrome.tabs.query({active: true, windowId: currentWindow.id}, function(tabs) {
+                    chrome.pageAction.show(tabs[0].id);
+                  });
+                });
+                break;
+              }
             } 
           }
         }
         return r;
     }
 
-	// Evaluate the different urls
+    function createTag(params) {
+      var result = "";
+      for( var i = 0; i < params.length; i++ ) {
+        result = result + params[i].param + "=" + params[i].paramValue;
+        if( i >= 0 && i < params.length - 1 ) {
+            result = result + "&";
+        }
+      }
+      return result;
+    }
+
   
-  if (!chrome.webRequest.onBeforeRequest.hasListener(addTag)) {   
+  if (!chrome.webRequest.onBeforeRequest.hasListener(addTag)) {      
    var site_urls = [ 
             "http://*.amazon.ca/*/dp/*",
             "http://*.amazon.ca/dp/*",
